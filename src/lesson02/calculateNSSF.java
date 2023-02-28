1a7886f1f0602b4b0f47ed182d5f593751e50f23
smatuku <smatuku@dewcis.com>
2023-02-28 12:11:37 +0300

    new rates

--- src/lesson02/calculateNSSF.java
+++ src/lesson02/calculateNSSF.java
@@ -9,18 +9,22 @@ public class calculateNSSF {
 		System.out.println("Enter Gross Pay (KES): ");
 		//Convert entered value to float and call function
 		float scannerValue = Float.parseFloat(scanner.nextLine());
-		getOldRate(scannerValue);
+		getRate(scannerValue);
 
 	}
 	
-	public static float getOldRate(float grossPay) {	
-		//Calculate NSSF
-		float nssfAmount = grossPay * 0.05f;
-		if(nssfAmount>200) {
-			nssfAmount = 200.0f;
+	public static float getRate(float grossPay) {	
+		//Calculate NSSF Old Rate
+		float nssfAmountOld = grossPay * 0.05f;
+		if(nssfAmountOld>200) {
+			nssfAmountOld = 200.0f;
 		}
-		System.out.println("NSSF Amount for Gross Pay KES"+grossPay+" is KES"+ nssfAmount);
-		return nssfAmount;
+		//Calculate NSSF New Rate
+		float nssfAmountNew = grossPay * 0.06f;
+		
+		System.out.println("Old Rate NSSF Amount for Gross Pay KES"+grossPay+" is KES"+ nssfAmountOld);
+		System.out.println("New Rate NSSF Amount for Gross Pay KES"+grossPay+" is KES"+ nssfAmountNew);
+		return nssfAmountOld;
 		
 	}
 
